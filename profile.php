af70c8c797035c04c081962d3faf159d5f824b3e
Muhammad Rahaji Jhaerol <59465944+rhjhaerol@users.noreply.github.com>
2022-10-20 13:51:53 +0700

    add profile-page

---
+++ profile.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Uni Pwt</title>
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js">
+    <script src="https://kit.fontawesome.com/0ac2a089e1.js" crossorigin="anonymous"></script>
+    <!-- My CSS -->
+    <link rel="stylesheet" href="css/styles.css">
+    <!-- Icon -->
+    <link rel="shortcut icon" href="assets/favicon.ico" type="image/x-icon">
+    <!-- Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  </head>
+  <body style="background-color: #F5F6FA;">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4" style="padding-top: 30px; padding-left: 60px;">
+                <div class="frame" style="width: 509px; height: 191px; margin-bottom: 30px;">
+                    <div style="text-align: center;">
+                        <h1 class="aaa" style="padding-top: 22px;">Your Points</h1>
+                        <h1 style="padding-top: 10px; font-family: Montserrat; font-size: 36;">200</h1>
+                        <a href="" style="font-family: Open Sans; font-size: 14px;">Get voucher here</a>
+                    </div> 
+                </div>
+                <div class="frame" style="width: 509px; height: 413px; margin-bottom: 30px">
+                    <div >
+                        <h1 class="aaa" style="padding-top: 30px; text-align: center;">How to get points?</h1>
+                        <div class="row" style="padding: 40px 50px;">
+                            <div class="col-1">
+                                <img src="assets\material-symbols_travel-explore.png" alt="">
+                            </div>
+                            <div class="col-11" style="margin-top: -2px; padding-left: 40px; padding-bottom: 20px;">
+                                <h1 style="font-family: Montserrat; font-size:24px;">Search</h1>
+                                <p style="font-family: Open Sans; font-size: 14px; margin-top: -10px;">Find an amazing destination</p>
+                            </div>
+                            <div class="col-1">
+                                <img src="assets/healthicons_travel.png" alt="">
+                            </div>
+                            <div class="col-11" style="margin-top: -2px; padding-left: 40px; padding-bottom: 20px;">
+                                <h1 style="font-family: Montserrat; font-size:24px;">Travel</h1>
+                                <p style="font-family: Open Sans; font-size: 14px; margin-top: -10px;">Go to the destination</p>
+                            </div>
+                            <div class="col-1">
+                                <img src="assets/subway_write.png" alt="" style="padding-left: 7px; padding-top:7px;">
+                            </div>
+                            <div class="col-11" style="margin-top: -2px; padding-left: 40px;">
+                                <h1 style="font-family: Montserrat; font-size:24px;">Review</h1>
+                                <p style="font-family: Open Sans; font-size: 14px; margin-top: -10px;">Write a review about that place</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="frame" style="width: 509px; height: 300px; margin-bottom: 10px">
+                    <div>
+                        <h1 class="aaa" style="padding-top: 30px; text-align: center;">Benefits of Review</h1>
+                        <div class="row" style="padding: 30px 50px;">
+                            <div class="col-1">
+                                <img src="assets/carbon_idea.png" alt="">
+                            </div>
+                            <div class="col-11" style="margin-top: -2px; padding-left: 40px; padding-bottom: 20px;">
+                                <h1 style="font-family: Montserrat; font-size:24px;">Insight</h1>
+                                <p style="font-family: Open Sans; font-size: 14px; margin-top: -10px;">Other people will know about 
+                                    the real conditions of the place</p>
+                            </div>
+                            <div class="col-1">
+                                <img src="assets/heroicons_ticket.png" alt="">
+                            </div>
+                            <div class="col-11" style="margin-top: -2px; padding-left: 40px; padding-bottom: 20px;">
+                                <h1 style="font-family: Montserrat; font-size:24px;">Voucher</h1>
+                                <p style="font-family: Open Sans; font-size: 14px; margin-top: -10px;">Get vouchers of writing a honest
+                                    review by your tier</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-8" style="padding-top: 30px; padding-left: 120px;">
+                <div class="frame" style="width: 843px; height: 325px; margin-bottom: 12px;">
+                    <div>
+                        <h1 class="aaa" style="padding-top: 30px; text-align: center;">Member Tier</h1>
+                    </div>
+                    <div class="row" style="text-align: center;">
+                        <div class="col-4" style="padding-left: 150px;">
+                            <h1 class="tier">Bronze</h1>
+                            <img src="assets\fluent-emoji-flat_3rd-place-medal.png" alt="">
+                            <h1 class="tier" style="color: red; padding-top:10px;">Your tier is here</h1>
+                        </div>
+                        <div class="col-4">
+                            <h1 class="tier">Silver</h1>
+                            <img src="assets\fluent-emoji-flat_2nd-place-medal.png" alt="">
+                            <h1 class="tier" style="color: rgba(34, 34, 34, 0.56); padding-top:10px;">Get 800 points more</h1>
+                        </div>
+                        <div class="col-4" style="padding-right: 150px;">
+                            <h1 class="tier">Gold</h1>
+                            <img src="assets\fluent-emoji-flat_1st-place-medal.png" alt="">
+                            <h1 class="tier" style="color: rgba(34, 34, 34, 0.56); padding-top:10px;">Get 1800 points more</h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="frame" style="width: 843px; height: 627px; margin-bottom: 10px;">
+                    <div>
+                        <h1 class="aaa" style="padding-top: 30px; text-align: center;">Member Card</h1>
+                    </div>
+                    <div class="card-member" style="margin-top: 30px;">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="lingkaran" style="margin: 45px 20px;">
+                                    <img src="assets/img/photos.png" alt="" style="padding: 8px;">
+                                </div>
+                            </div>
+                            <div class="col-6" style="margin-left: -20px;">
+                                <h1 style="font-family: Montserrat; font-size: 24px; padding-top: 55px; color: #FFFFFF;">Dian Maharani</h1>
+                                <h2 style="font-family: Open Sans; font-size: 14px; color: #FFFFFF; padding-left: 65px;">Bronze</h2>
+                                <h2 style="font-family: Open Sans; font-size: 13px; color: #FFFFFF; margin-top: 30px;">dianmaharani2003@gmail.com</h2>
+                                <h2 style="font-family: Open Sans; font-size: 13px; color: #FFFFFF;">+62 81327108489</h2>
+                                <h2 style="font-family: Open Sans; font-size: 13px; color: #FFFFFF;">Purwokerto</h2>
+                            </div>
+                        </div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+  </body>
+</html>
